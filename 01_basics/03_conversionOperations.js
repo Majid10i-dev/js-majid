9ff589978237fa8c9873e6c67470181718dffa14
Majid10i-dev <mohammedmajidnlg@gmail.com>
2025-06-05 21:15:52 +0000

    Operations and Conversion of Data Types

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
 let score = true
  
-console.log(typeof score)
+// console.log(typeof score)
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33"     => 33
 // "33abc"  => NaN (Not an Number)
@@ -14,8 +14,8 @@ let isLoggedIn = 1
 
 let valueInBool = Boolean(isLoggedIn)
 
-console.log(valueInBool)
-console.log(typeof valueInBool)
+// console.log(valueInBool)
+// console.log(typeof valueInBool)
 
 // 1        =>  true;      0 => false
 // ""       =>  false
@@ -23,5 +23,29 @@ console.log(typeof valueInBool)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//      ----- Some Operations Here -----
+
+let str1 = "Hello"
+let str2 = " Majid"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// if string is first then it will be added string and no operations will be performed
+// console.log("2" + 2 + 2)
+
+// if there are operations first then it will be performed first it will be converted to string
+// console.log(2 + 2 + "2")
+
+// Avoid writing code like below, it ruins the readability
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
