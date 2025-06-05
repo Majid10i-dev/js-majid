b603e186b44a0c6a0ef25359d27bf536bd43fcd9
Majid10i-dev <mohammedmajidnlg@gmail.com>
2025-06-05 18:10:22 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as never version
+
+// alert(3+3) we are nodejs not browser
+
+console.log(3 + 
+    3) //code readabilty should be high
+
+let name = "majid"
+let age = 19
+let isLoggedIn = false
+let city //undefined
+
+/* Types of DataTypes
+->number        =>     ranges from 2 to 2^52-1
+->bigInt        =>     ranges beyond number
+->String        =>     use single quote ('') or double qoute ("")
+->null          =>     Standalone value
+->Undefined     => 
+->symbol
+
+->object        =>
+*/
+
+console.log(typeof age)
+console.log(city)
