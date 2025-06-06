40e1541db082e1d18c4bb6a335fe015b3f06ada4
Majid10i-dev <mohammedmajidnlg@gmail.com>
2025-06-06 11:54:18 +0000

    Stack vs Heap

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -17,3 +17,31 @@ console.log(2 === "2") //false
 console.log(undefined == 0)
 console.log(undefined >= 0)
 console.log(undefined <= 0)
+
+// ------ Stacks vs Heap ------
+
+// Stack:
+
+let myId = 2
+let anotherId = myId
+anotherId = 3
+
+console.log(myId)
+console.log(anotherId)
+
+// changing anotherId won't change myId because anotherId has just copy of myId
+
+// Heap:
+
+let userOne = {
+    email : "majid@google.com",
+    pass : 123321
+};
+
+let userTwo = userOne
+userTwo.email = "majid@outlook.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+//Now both userOne and UserTwo are pointing to same object so changing any value will reflects original values
